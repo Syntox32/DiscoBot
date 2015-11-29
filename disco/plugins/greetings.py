d603656004b5ed1fb161c8da5e01412bd82fdca5
Syntox32 <syntox32@gmail.com>
2015-11-29 18:20:55 +0100

    simpler example

--- disco/plugins/greetings.py
+++ disco/plugins/greetings.py
@@ -15,31 +15,23 @@ class GreetingPlugin(Plugin):
 	title = "Greeting plugin"
 	desc = "What is my purpose?"
 
-	def __init__(self): pass
+	def __init__(self): 
+		pass
 
 	def on_ready(self, client):
 		log.info("Greeting plugin loaded")
 
-	def on_message(self, client, message): pass
+	def on_message(self, client, message): 
+		pass
 
-	def on_member_join(self, client, member):
+	def on_member_join(self, client, member): 
 		# get the default text-channel
 		channel = member.server.get_default_channel()
 
-		mems = member.server.members
-		found = False
-		for m in mems:
-			if m.id == member.id:
-				found = True
-
-		if True:
-			client.send_message(channel, "Welcome to the guild, %s!" % member.name)
-		else:
-			client.send_message(channel, "You lill shit you think u can leave like that, %s" % member.name)
-
-	def on_voice_state_update(self, client, member):
-		"""
-		Is called when a member joins, leaves, is muted or is deafend in a channel
-		"""
+		client.send_message(channel, "Welcome to the guild, %s!" % member.name)
+	
+	def on_member_remove(self, client, member): 
 		# get the default text-channel
 		channel = member.server.get_default_channel()
+
+		client.send_message(channel, "You lill shit you think u can leave like that, %s" % member.name)
