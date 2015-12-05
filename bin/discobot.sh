16f4662115d1e9dd389ed6b34269a54118f8b28d
Syntox32 <syntox32@gmail.com>
2015-12-06 00:29:20 +0100

    made setup.py actually work

--- bin/discobot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Starting DiscoBot..."
-python ./main.py
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
 	author="syntox",
 	author_email="syntox32@gmail.com",
 	url="https://github.com/syntox32/discobot",
+	include_package_data=True,
 	packages=["disco"],
-	scripts=["bin/run.sh"]
+	scripts=["bin/discobot.sh", "bin/start-disco.py"]
 )
