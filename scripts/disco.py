78dc92ee43f4496d32009d9630eaeb429f236a23
Syntox32 <syntox32@gmail.com>
2015-12-07 18:30:53 +0100

    moved start-script and plugin_gen.py to own folder

---
+++ scripts/disco.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+"""
+DiscoBot by Syntox
+	https://github.com/syntox32
+
+Discord Python wrapper:
+	GitHub: https://github.com/Rapptz/discord.py
+	API Docs: rapptz.github.io/discord.py/api.html
+
+"""
+
+import threading, sys, disco
+
+def main():
+	bot = disco.DiscoBot()
+	bot.connect(disco.creds["email"], 
+		disco.creds["pass"])
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		print "Interrupted."
+		log.info("KeyboardInterrupt.. exiting.")
+		sys.exit(0)
