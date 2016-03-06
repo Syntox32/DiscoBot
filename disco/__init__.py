fff653bb6dd02297dcc8b31a3e8165d4a256630b
Syntox32 <syntox32@gmail.com>
2016-03-06 01:56:37 +0100

    refactored config

--- disco/__init__.py
+++ disco/__init__.py
@@ -1,2 +1,13 @@
-from .config import creds
+# -*- coding: utf-8 -*-
+"""
+DiscoBot the Amazing Chat Companion
+"""
+
+from .config import Config
 from .discobot import DiscoBot
+from .utils import configure_logger
+
+# Configure logging
+configure_logger("disco", level=Config.LOGGING_LEVEL)
+# Setup log-to-file-only for the discord.py logger
+configure_logger("discord", stream=False, level=Config.LOGGING_LEVEL)
