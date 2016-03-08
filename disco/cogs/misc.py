908fb46c1b5925b3392064ed177bced3dce0d2b7
Syntox32 <syntox32@gmail.com>
2016-03-08 21:34:06 +0100

    added more stupid things

--- disco/cogs/misc.py
+++ disco/cogs/misc.py
@@ -14,8 +14,18 @@ class Misc:
         self.bot = bot
         self.key = self.__class__.__name__
 
+    async def on_message(self, message: Message):
+        """lol"""
+        me = message.server.me if message.channel.is_private else self.bot.user
+
+        if me in message.mentions:
+            msg = message.content.lower()
+            if any([True for k in bad_words if k in msg]):
+                await self.bot.send_message(message.channel, random.choice(responses))
+
     @commands.command(name="8ball", pass_context=True, no_pm=True, aliases=["8"])
     async def eightball(self, ctx, whatdo: str):
+        """Receive well thought-out wisdom"""
         wise_words = random.choice(wisdom)
         # just make sure we don't get
         # the same twice in a row
@@ -31,35 +41,59 @@ def setup(bot):
     bot.add_cog(Misc(bot))
 
 wisdom = [
-	"It is certain",
-	"It is decidedly so",
-	"Without a doubt",
-	"Yes, definitely",
-	"Most likely",
-	"Yes",
-	"Signs point to yes",
-    #"You may rely on it",
-    #"As I see it, yes",
-    #"Outlook good",
+"It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes, definitely",
+"Most likely",
+"Yes",
+"Signs point to yes",
+"BY THE GODS, YES!"
 
-	#"Reply hazy try again",
-	#"Ask again later",
-	#"Better not tell you now",
-	#"Cannot predict now",
-	#"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful",
+"Absolutely no fucking way",
+"You're alone in this",
 
-	"Don't count on it",
-	"My reply is no",
-	"My sources say no",
-	"Outlook not so good",
-	"Very doubtful",
-    "Absoloutly fucking no way",
+"Lol",
+"Perv",
+"There are children starving and this is what you question in this world?",
+"Get out of the basement once in a while",
+"Better not tell you",
+"Kappa",
+"RIP",
+"rekt",
+"( ͡° ͜ʖ ͡°)",
 
-    "Lol",
-    "Perv",
-    "There are children starving and this is what you question in this world?",
-    "Get out of the basement once in a while",
-    "Better not tell you",
-    "Kappa",
-    "RIP",
+#"You may rely on it",
+#"As I see it, yes",
+#"Outlook good",
+
+#"Reply hazy try again",
+#"Ask again later",
+#"Better not tell you now",
+#"Cannot predict now",
+#"Concentrate and ask again",
+]
+
+bad_words = [
+"fuck", "fck", "fak", "fuk",
+"shit", "shiet", "hate", "dipshit",
+"faen", "helvette", "dust", "drit",
+"cock", "penis", "vagina", "your mom",
+"ur mom", "daym", "damn", "dammit",
+"wtf", "wth", "hell", "suck", "dick",
+]
+
+responses = [
+"What the fuck did you say about me you li'll bitch?",
+"Well fuck you too",
+"Blow me",
+"Love u<3",
+"That's my boy",
+"Nice to see you too",
+"Easy on the language, kid"
 ]
