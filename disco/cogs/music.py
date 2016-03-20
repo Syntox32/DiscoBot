e7fac1cf63938086cf833ed45b7d45839352eeb0
Syntox32 <syntox32@gmail.com>
2016-03-20 11:54:04 +0100

    reworked the music cog a bit

--- disco/cogs/music.py
+++ disco/cogs/music.py
@@ -14,14 +14,27 @@ class Music:
     def __init__(self, bot):
         self.bot = bot
         self.key = self.__class__.__name__
+
         self.follow = True
         self.vclients = {}
+        self.listeners = []
+        self.rtv = None
+        self.playlist = []
+        self.loop = False
 
-    async def join_owner_on_server(self, server):
+    async def on_ready(self):
+        logger.info("Opus loaded: {}".format(opus.is_loaded()))
+
+        if self.follow:
+            logger.info("Follow enabled.")
+            for server in self.bot.servers:
+                await self.join_member(server, Config.OWNER_ID)
+
+    async def join_member(self, server, user_id):
         """Given a server, seek out the owner and join
         the current voice channel he is in, if it exists.
         """
-        member = utils.get(server.members, id=Config.OWNER_ID)
+        member = utils.get(server.members, id=user_id)
         if member is not None:
             logger.debug("Found member: {}".format(member.name))
             vchan = member.voice_channel
@@ -37,33 +50,37 @@ class Music:
         else:
             logger.debug("Member not found.")
 
-    @commands.command(name="follow", pass_context=True, no_pm=True, aliases=[])
-    async def follow_owner(self, ctx):
-        await self.join_owner_on_server(ctx.message.server)
+    def get_player(self, ctx):
+        voice = self.vclients[ctx.message.server.id]
+        return voice["player"]
 
-    async def on_ready(self):
-        logger.info("Opus loaded: {}".format(opus.is_loaded()))
+    def get_client(self, ctx):
+        voice = self.vclients[ctx.message.server.id]
+        return voice["client"]
 
-        if self.follow:
-            logger.info("Follow enabled.")
-            for server in self.bot.servers:
-                await self.join_owner_on_server(server)
+    async def init_player_with_url(self, server, url):
+        voice = self.vclients[server.id]
+        voice["player"] = await voice["client"].create_ytdl_player(url)
+        logger.info("Created new player ({}).".format(voice.channel.name))
+        return voice["player"]
 
-    @commands.command(pass_context=True, no_pm=True, aliases=[])
+    @commands.command(pass_context=True, no_pm=True)
+    async def follow(self, ctx):
+        await self.join_owner_on_server(ctx.message.server, ctx.message.author.id)
+
+    @commands.command(pass_context=True, no_pm=True)
     async def play(self, ctx, url : str):
-        voice = self.vclients[ctx.message.server.id]
-        voice["player"] = await voice["client"].create_ytdl_player(url) #"https://www.youtube.com/watch?v=N9qYF9DZPdw")
-        voice["player"].start()
+        player = await self.init_player_with_url(ctx.message.server, url)
+        player.start()
 
-    @commands.command(pass_context=True, no_pm=True, aliases=[])
+    @commands.command(pass_context=True, no_pm=True)
     async def stop(self, ctx):
-        voice = self.vclients[ctx.message.server.id]
-        voice["player"].stop()
+        player = get_player(ctx)
+        player.stop()
 
     @commands.command(pass_context=True, no_pm=True, aliases=["q", "que"])
     async def queue(self, ctx, url : str):
-        voice = self.vclients[ctx.message.server.id]
-        player = voice["player"]
+        player = get_player(ctx)
         if player.is_playing():
             player.stop()
             logger.debug("Stopping player in progress")
