bfc85a93b97b0a6b6640eaaa08eae6fad7788aba
Anders Milje <syntox32@gmail.com>
2017-02-25 21:50:54 +0100

    removed 'interactive console' from main.py

--- main.py
+++ main.py
@@ -1,50 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 """
 Disco the Amazing Chat Companion!
-By Syntox <github.com/syntox32> - I'm also on Discord @syn#1389
+By Syntox <github.com/syntox32> - Also on Discord @syn#1389
 
 Built using Rapptz's Discord API wrapper
 Which can be found here: github.com/Rapptz/discord.py
 """
 
-import asyncio, logging, argparse, os
+from disco.config import Config
 from disco.discobot import bot
-from discord.ext import commands
-
-async def command_listener(bot: commands.Bot):
-    """
-    Command listener things, used for CLI interaction
-    """
-    await bot.wait_until_ready()
-    print("Welcome to interactive mode, {0}.\n ".format(bot.user.name) \
-        + "Commands: e|exit, acceptinv <invite>, leaveall")
-    while True:
-        cmd = input("#> ")
-        if cmd in ("exit", "e"):
-            print("Exiting...")
-            await bot.logout()
-            print("Logged out.")
-            break
 
 if __name__ == "__main__":
-    """
-    Main entry point.
-    """
-    parser = argparse.ArgumentParser(description="Disco the Amazing Chat Companion!")
-    parser.add_argument("-i", "--interactive", action="store_true",
-        help="Starts DiscoBot in interactive mode, for your needs and purposes." \
-        + "Blocks other output from showing.")
-    args = parser.parse_args()
-
-    #loop = asyncio.get_event_loop()
-
-    bot.run(os.environ.get("DISCOBOT_TOKEN", None))
-
-    #try:
-    #	if args.interactive:
-    #		loop.create_task(command_listener(bot))
-    #	loop.run_until_complete(bot.run(os.environ.get("DISCOBOT_TOKEN", None)))
-    #except Exception as e:
-    #	loop.run_until_complete(bot.logout())
-    #finally:
-    #	loop.close()
+    try:
+        bot.run(Config.DISCORD_TOKEN)
+    except Exception as e:
+        print(e)
